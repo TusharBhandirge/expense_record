25dc6af7d7c01a8eb5fb2df586b3d464e9c87ff4
TusharBhandirge <tusharbhandirge@gmail.com>
2021-04-27 21:35:42 +0530

    Update  :  storing all expenses in array of object

--- recordExpense.js
+++ recordExpense.js
@@ -2,14 +2,27 @@
 
 var addButton = document.querySelector("#button-add");
 var divOutput = document.querySelector("#div-output");
-var divInput = document.querySelector("#input-id");
+var inputAmount = document.querySelector("#input-id-amount");
+var inputDesc = document.querySelector("#input-id-desc");
+const allExpenses = [];
 var count = 0;
 
 
 
 function addClicked(){
-    var input=divInput.value;
-    const expense = parseInt(input,10);
+
+    var expenseObj = {};
+    var amount=inputAmount.value;
+    var desc = inputDesc.value;
+
+    expenseObj.amount = amount;
+    expenseObj.desc = desc;
+
+    allExpenses.push(expenseObj);
+    console.clear();
+    console.table(allExpenses);
+
+    const expense = parseInt(amount,10);
     count = count+expense;
     divOutput.textContent = count;
 }

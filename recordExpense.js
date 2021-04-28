fdba2215321db2a6a5b656c436bb59e9c9860e89
TusharBhandirge <tusharbhandirge@gmail.com>
2021-04-28 17:24:38 +0530

    Update  : Added map to allExpenses

--- recordExpense.js
+++ recordExpense.js
@@ -4,6 +4,7 @@ var addButton = document.querySelector("#button-add");
 var divOutput = document.querySelector("#div-output");
 var inputAmount = document.querySelector("#input-id-amount");
 var inputDesc = document.querySelector("#input-id-desc");
+var expenseEle = document.querySelector('#id-Expense-table');
 const allExpenses = [];
 var count = 0;
 
@@ -25,6 +26,16 @@ function addClicked(){
     const expense = parseInt(amount,10);
     count = count+expense;
     divOutput.textContent = count;
+
+    const expenseEleArr = allExpenses.map((item)=>{
+        return `<div> ${item.amount} :: ${item.desc}`;
+    });
+
+    const expenseEleHtmlStr = expenseEleArr.join("");
+
+
+    expenseEle.innerHTML = expenseEleHtmlStr;
+
 }
 
 addButton.addEventListener("click",addClicked);
